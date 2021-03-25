3655402e4e09d85067f84aaa6515fe83b9323c4f
乐洋 <17377416@buaa.edu.cn>
2021-03-25 11:09:27 +0800

    create dir script_tools to store scripts, and add scripts for manufacturing titles

---
+++ script_tools/make_function_description_title.py
@@ -0,0 +1,21 @@
+from contextlib import redirect_stdout
+import sys
+
+def manufacture_title():
+    TITLE_PREFIX = '3.3.3'
+    FUNC_LVL = '#' * 5
+    FUNC_NUM = 6
+    DETAIL_LEVEL = FUNC_LVL + '#'
+    for i in range(1, FUNC_NUM+1):
+        print(f'{FUNC_LVL} {TITLE_PREFIX}.{i}')
+        print(f'{DETAIL_LEVEL} {TITLE_PREFIX}.{i}.1 功能描述')
+        print(f'{DETAIL_LEVEL} {TITLE_PREFIX}.{i}.2 操作流程')
+        print(f'{DETAIL_LEVEL} {TITLE_PREFIX}.{i}.3 输入')
+        print(f'{DETAIL_LEVEL} {TITLE_PREFIX}.{i}.4 输出')
+        print(f'{DETAIL_LEVEL} {TITLE_PREFIX}.{i}.5 约束与约定')
+
+
+f = sys.stdout
+with redirect_stdout(f):
+    manufacture_title()
+
